ebd671236f2d8845f1a5ca42e1749e0857e35c04
Andres <and.vargasg@duocuc.cl>
2026-05-08 19:02:45 -0400

    Estructura del proyecto

---
+++ src/test/java/com/ProyectoSemestral/ProyectoSemestralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoSemestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemestralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
