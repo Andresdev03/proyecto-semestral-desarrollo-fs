b70010f5785287f4e7b6ae4c5bee4badf5f9b010
Bocchi-ghub <sebastian.mautz@gmail.com>
2026-05-14 23:23:58 -0400

    Contrato Model

--- src/main/java/com/ProyectoSemestral/Contrato/Model/ContratoModel.java
+++ src/main/java/com/ProyectoSemestral/Contrato/Model/ContratoModel.java
@@ -1,4 +1,32 @@
 package com.ProyectoSemestral.Contrato.Model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class ContratoModel {
+    @NotNull
+    int id_contrato;
+    @NotBlank(message = "El contrato debe tener un tipo.")
+    String tipoContrato;
+    @NotBlank(message = "El contrato debe tener un cargo.")
+    String cargoContrato;
+    @NotNull(message = "El contrato debe tener fecha de inicio.")
+    Date fecIniContrato;
+    // nullable
+    Date fecFinContrato;
+    int salarioContrato;
+    // run emp
+    // id sucursal
+
+
 }
