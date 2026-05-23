ca17dcce90738d53998bbc205b63280e69194a10
Bocchi-ghub <sebastian.mautz@gmail.com>
2026-05-23 01:20:10 -0400

    Primeras correcciones para testeo de nueva estructura.

--- Contrato/src/main/java/org/example/contrato/Model/ContratoModel.java
+++ Contrato/src/main/java/org/example/contrato/Model/ContratoModel.java
@@ -1,4 +1,31 @@
 package org.example.contrato.Model;
 
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@AllArgsConstructor
+@Data
+//@NoAllArgsConstructor
+
 public class ContratoModel {
+   // @NotNull
+    int id_contrato;
+    //@NotBlank(message = "El contrato debe tener un tipo.")
+    String tipoContrato;
+    //@NotBlank(message = "El contrato debe tener un cargo.")
+    String cargoContrato;
+    //@NotNull(message = "El contrato debe tener fecha de inicio.")
+    Date fecIniContrato;
+    // nullable
+    Date fecFinContrato;
+    int salarioContrato;
+    // run emp
+    // id sucursal
+
 }
