46bd646b9b5e745ec2eac36263dc5e1bc9dc3cf5
Bocchi-ghub <sebastian.mautz@gmail.com>
2026-05-23 17:14:25 -0400

    Microservicio de contrato v1

--- Contrato/src/main/java/org/example/contrato/Controller/ContratoController.java
+++ Contrato/src/main/java/org/example/contrato/Controller/ContratoController.java
@@ -1,9 +1,16 @@
 package org.example.contrato.Controller;
 
 
+import jakarta.validation.Valid;
+import org.example.contrato.Model.Contrato;
+import org.example.contrato.Service.ContratoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contratos")
@@ -11,6 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class ContratoController {
 
+
+    @Autowired
+    private ContratoService contratoService;
+
+    @PostMapping
+    public ResponseEntity<Contrato> registrar(@Valid @RequestBody Contrato contrato){
+        contratoService.registrar(contrato);
+        return new ResponseEntity<>(contrato, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Contrato>> listarTodos(){
+        List<Contrato> lista = contratoService.listarTodos();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
+    }
+
+    @GetMapping("/run/{runEmpleado}")
+    public ResponseEntity<List<Contrato>> buscarPorRun(@PathVariable String runEmpleado){
+        List<Contrato> listaEncontrados = contratoService.buscarPorRun(runEmpleado);
+        return new ResponseEntity<>(listaEncontrados, HttpStatus.OK);
+    }
+
+    @GetMapping("/salario/{salario}")
+    public ResponseEntity<List<Contrato>> buscarPorSalario(@PathVariable BigDecimal salario){
+        List<Contrato> listaEncontrados = contratoService.buscarPorSalario(salario);
+        return new ResponseEntity<>(listaEncontrados, HttpStatus.OK);
+    }
 
 
 
