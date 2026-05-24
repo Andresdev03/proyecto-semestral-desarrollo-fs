b8f16adca2bc1419b76dc29ed4cb1cea9dda8322
Bocchi-ghub <sebastian.mautz@gmail.com>
2026-05-24 01:48:45 -0400

    Microservicio boleta funcional v1

---
+++ Boleta/src/main/java/org/example/boleta/Model/Boleta.java
@@ -0,0 +1,51 @@
+package org.example.boleta.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "boleta")
+public class Boleta {
+
+    @Id
+    @NotNull(message = "Debe incluir un ID.")
+    @Column(name = "id_boleta")
+    private Integer idBoleta;
+
+    @NotNull(message = "Debe incluir una fecha de creacion")
+    @Column(name = "fec_creacion_boleta")
+    private LocalDate fecCreacionBoleta;
+
+    @NotNull(message = "Debe incluir un total en la boleta.")
+    @Positive
+    @Column(name = "valor_total_boleta")
+    private BigDecimal valorTotalBoleta;
+
+    @NotBlank(message = "Debe incluir un método de pago")
+    @Column(name = "metodo_pago_boleta")
+    private String metodoPagoBoleta;
+
+    @NotNull
+    @Column(name = "id_pedido")
+    private Integer idPedido;
+
+
+
+}
--- Boleta/src/main/java/org/example/boleta/Service/BoletaService.java
+++ Boleta/src/main/java/org/example/boleta/Service/BoletaService.java
@@ -1,4 +1,65 @@
 package org.example.boleta.Service;
 
+import org.example.boleta.Model.Boleta;
+import org.example.boleta.Repository.BoletaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class BoletaService {
+
+    @Autowired
+    private BoletaRepository boletaRepository;
+
+    public Boleta registrar(Boleta boleta){
+        return boletaRepository.save(boleta);
+    }
+
+
+    public List<Boleta> listarTodas(){
+        return boletaRepository.findAll();
+    }
+
+    public List<Boleta> buscarPorFecha(LocalDate fechaCreacionBoleta){
+        return boletaRepository.findByFecCreacionBoleta(fechaCreacionBoleta);
+    }
+
+    public List<Boleta> buscarPorMetodoPago(String metodoPagoBoleta){
+        return boletaRepository.findByMetodoPagoBoleta(metodoPagoBoleta);
+    }
+
+    public Boleta buscarPorId(Integer idBoleta){
+        return boletaRepository.findById(idBoleta).orElse(null);
+    }
+
+    public Boleta modificarPorId(Integer idBoleta, Boleta boletaModificada){
+        Boleta boletaOriginal = buscarPorId(idBoleta);
+        if (boletaOriginal == null){
+            return null;
+        }
+
+        boletaOriginal.setFecCreacionBoleta(boletaModificada.getFecCreacionBoleta());
+        boletaOriginal.setValorTotalBoleta(boletaModificada.getValorTotalBoleta());
+        boletaOriginal.setMetodoPagoBoleta(boletaModificada.getMetodoPagoBoleta());
+        boletaOriginal.setIdPedido(boletaModificada.getIdPedido());
+        return boletaRepository.save(boletaOriginal);
+
+
+
+    }
+
+    public boolean eliminarPorId(Integer idBoleta){
+        Boleta boleta = buscarPorId(idBoleta);
+        if (boleta == null){
+            return false;
+        }
+
+        boletaRepository.deleteById(idBoleta);
+        return true;
+    }
+
+
 }
