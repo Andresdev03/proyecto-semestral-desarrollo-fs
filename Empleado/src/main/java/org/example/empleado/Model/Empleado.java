98f8c506f0d86b3cac4e19c6d9a7b627cee8d584
Bocchi-ghub <sebastian.mautz@gmail.com>
2026-06-11 01:16:05 -0400

    Empleado: Documentación del modelo y controller con swagger ya funcionales, también hice algunas modificaciones al pom y a las properties para que se ajusten a MySQL y XAMPP.

--- Empleado/src/main/java/org/example/empleado/Model/Empleado.java
+++ Empleado/src/main/java/org/example/empleado/Model/Empleado.java
@@ -1,6 +1,7 @@
 package org.example.empleado.Model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -24,45 +25,54 @@ public class Empleado {
 
     @Id
     @NotBlank(message = "Debe incluir run.")
-    @Size(min = 7, max = 8)
+    @Size(min = 8, max = 9)
+    @Schema(description = "Run del empleado, sin DV.", example = "20680568")
     @Column(name = "run_empleado")
     private String runEmpleado;
 
     @NotBlank(message = "Debe incluir número verificador.")
     @Size(min = 1, max = 1)
+    @Schema(description = "Dígito verificador del RUN.", example = "k")
     @Column(name = "dv_run_empleado")
     private String dvRunEmpleado;
 
     @NotBlank(message = "Debe incluir un correo.")
     @Email(message = "Debe cumplir con el formato de un correo.")
     @Size(max = 100)
+    @Schema(description = "Correo del empleado.", example = "sebastian.mautz@gmail.com")
     @Column(name = "correo_empleado", nullable = false, unique = true, length = 100)
     private String correoEmpleado;
 
     @NotBlank(message = "Debe incluir un teléfono.")
     @Size(max = 12)
+    @Schema(description = "Número de contacto del empleado.", example = "+56923567809")
     @Column(name = "telefono_empleado", unique = true, length = 12)
     private String telefonoEmpleado;
 
     @NotBlank(message = "Debe incluir un nombre.")
     @Size(max = 50)
+    @Schema(description = "Primer nombre del empleado.", example = "Sebastian")
     @Column(name = "pnombre_empleado")
     private String pnombreEmpleado;
 
     @Size(max = 50)
+    @Schema(description = "Segundo nombre del empleado.", example = "Ignacio")
     @Column(name = "snombre_empleado")
     private String snombreEmpleado;
 
     @NotBlank(message = "Debe incluir un apellido paterno.")
     @Size(max = 50)
+    @Schema(description = "Apellido paterno del empleado.", example = "Mautz")
     @Column(name = "appaterno_empleado")
     private String appaternoEmpleado;
 
     @Size(max = 50)
+    @Schema(description = "Apellido materno del empleado.", example = "Parham")
     @Column(name = "apmaterno_empleado")
     private String apmaternoEmpleado;
 
     @NotNull(message = "Debe incluir una fecha de nacimiento.")
+    @Schema(description = "Fecha de nacimiento del empleado.", example = "2001-09-13")
     @Column(name = "fecha_nac_empleado")
     private LocalDate fechaNacEmpleado;
 
