3b1161b361d224fc68821262e8ad9ba83727224d
Bocchi-ghub <sebastian.mautz@gmail.com>
2026-06-16 00:09:19 -0400

    Contrato y Boleta: Implementación de documentación en controller y modelo, correción en properties y pom para trabajar con swagger + MySQL para ambos microservicios.

--- Boleta/src/main/java/org/example/boleta/Model/Boleta.java
+++ Boleta/src/main/java/org/example/boleta/Model/Boleta.java
@@ -1,5 +1,6 @@
 package org.example.boleta.Model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -26,23 +27,28 @@ public class Boleta {
 
     @Id
     @NotNull(message = "Debe incluir un ID.")
+    @Schema(description = "ID de la boleta.", example = "1")
     @Column(name = "id_boleta")
     private Integer idBoleta;
 
     @NotNull(message = "Debe incluir una fecha de creacion")
+    @Schema(description = "Fecha de creacion de la boleta.", example = "2026-06-11")
     @Column(name = "fec_creacion_boleta")
     private LocalDate fecCreacionBoleta;
 
     @NotNull(message = "Debe incluir un total en la boleta.")
     @Positive
+    @Schema(description = "Valor total de la boleta.", example = "10300")
     @Column(name = "valor_total_boleta")
     private BigDecimal valorTotalBoleta;
 
     @NotBlank(message = "Debe incluir un método de pago")
+    @Schema(description = "Describe el método de pago utilizado por el cliente en esta boleta.", example = "Efectivo")
     @Column(name = "metodo_pago_boleta")
     private String metodoPagoBoleta;
 
     @NotNull
+    @Schema(description = "ID del pedido asociado a la boleta.", example = "1234")
     @Column(name = "id_pedido")
     private Integer idPedido;
 
--- Contrato/src/main/java/org/example/contrato/Controller/ContratoController.java
+++ Contrato/src/main/java/org/example/contrato/Controller/ContratoController.java
@@ -1,6 +1,10 @@
 package org.example.contrato.Controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.example.contrato.Model.Contrato;
 import org.example.contrato.Service.ContratoService;
@@ -14,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contratos")
-
+@Tag(name = "Empleado", description = "Operaciones asociadas a empleado.")
 
 public class ContratoController {
 
@@ -22,31 +26,54 @@ public class ContratoController {
     @Autowired
     private ContratoService contratoService;
 
+    @Operation(summary = "Registrar un contrato nuevo.", description = "Registra el contrato y lo retorna.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contrato registrado exitosamente."),
+            @ApiResponse(responseCode = "400", description = "Contrato no registrado por datos inválidos.")
+    })
     @PostMapping
     public ResponseEntity<Contrato> registrar(@Valid @RequestBody Contrato contrato){
         contratoService.registrar(contrato);
         return new ResponseEntity<>(contrato, HttpStatus.CREATED);
     }
 
-
+    @Operation(summary = "Listar todos los contratos.", description = "Retorna una lista de todos contratos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operacion exitosa.")
+    })
     @GetMapping
     public ResponseEntity<List<Contrato>> listarTodos(){
         List<Contrato> lista = contratoService.listarTodos();
         return new ResponseEntity<>(lista, HttpStatus.OK);
     }
 
+    @Operation(summary = "Buscar contratos por run de empleados.", description = "Retorna una lista de contratos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contrato encontrado."),
+            @ApiResponse(responseCode = "404", description = "Contrato no encontrado.")
+    })
     @GetMapping("/run/{runEmpleado}")
     public ResponseEntity<List<Contrato>> buscarPorRun(@PathVariable String runEmpleado){
-        List<Contrato> listaEncontrados = contratoService.buscarPorRun(runEmpleado);
+        List<Contrato> listaEncontrados = contratoService.buscarPorRunEmpleado(runEmpleado);
         return new ResponseEntity<>(listaEncontrados, HttpStatus.OK);
     }
 
+    @Operation(summary = "Buscar contratos por salario de empleados.", description = "Retorna una lista de contratos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contrato encontrado."),
+            @ApiResponse(responseCode = "404", description = "Contrato no encontrado.")
+    })
     @GetMapping("/salario/{salario}")
     public ResponseEntity<List<Contrato>> buscarPorSalario(@PathVariable BigDecimal salario){
         List<Contrato> listaEncontrados = contratoService.buscarPorSalario(salario);
         return new ResponseEntity<>(listaEncontrados, HttpStatus.OK);
     }
 
+    @Operation(summary = "Modificar contratos por su ID.", description = "Modifica el contrato y luego lo retorna.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contrato modificado."),
+            @ApiResponse(responseCode = "404", description = "Contrato no modificado.")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Contrato> modificarPorId(@PathVariable Integer id, @Valid @RequestBody Contrato contrato){
         Contrato contratoNuevo = contratoService.modificarPorId(id, contrato);
@@ -60,6 +87,11 @@ public class ContratoController {
 
     }
 
+    @Operation(summary = "Buscar contratos por ID.", description = "Retorna un contrato según ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contrato encontrado."),
+            @ApiResponse(responseCode = "404", description = "Contrato no encontrado.")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Contrato> buscarPorId(@PathVariable Integer id){
         Contrato contrato = contratoService.buscarPorId(id);
@@ -70,6 +102,11 @@ public class ContratoController {
         return new ResponseEntity<>(contrato, HttpStatus.OK);
     }
 
+    @Operation(summary = "Elimina el contrato según su ID.", description = "Si elimina el contrato, retorna no_content, si no lo elimina, retorna not_found.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Contrato eliminado exitosamente."),
+            @ApiResponse(responseCode = "404", description = "Contrato no encontrado.")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPorId(@PathVariable Integer id){
         boolean eliminado = contratoService.eliminarPorId(id);
